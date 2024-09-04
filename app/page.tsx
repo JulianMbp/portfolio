22dfd136107bc9ce39120aa53b96c97f978aa2aa
julian <julilito09@gmail.com>
2024-09-04 09:38:18 -0500

    commit3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className='flex justify-center items-center w-screen h-screen bg-cyan-600'>
-      <h1 className='text-4x1 text-cyan-800 font-extrabold'></h1>
+      <h1 className='text-4x1 text-cyan-800 font-extrabold'>My portafolio</h1>
     </main>
   );
 }
