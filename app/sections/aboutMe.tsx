5881e6d1307f45946f3c28dc76d5b53209f5e573
JulianMbp <julian_mtb993@outlook.es>
2024-10-23 12:26:34 -0500

    agregar imagenes en aboutMe

--- app/sections/aboutMe.tsx
+++ app/sections/aboutMe.tsx
@@ -1,18 +1,22 @@
+import ImgAbout from "../img/Imagen_About_Me.jpg"
+import Image from "next/image"
+
+
 const aboutMe = () => {
     return (
-        <div>
+        <div className="border">
             {/* About Me */}
-        <section className=" pl-2 grid grid-cols-1 md:grid-cols-2 justify-center items-center  " id='about'>
-            
-            <h1 className='font-extrabold text-8xl md:text-9xl order-1 ml-10 text-slate-50 border flex-wrap'>About <br /> Me</h1>
-            <p className="text-3xl order-3 pl-10 text-slate-50 col-span-2 md:col-span-1">¡Hola! 👋
-                Soy Julián M. Bastidas Pérez, ingeniero de software en formación 🧑‍💻. <br />
-                Me encanta crear cosas innovadoras y llevar ideas al siguiente nivel 🚀. <br />
+        <section className=" pl-2 grid grid-cols-1 md:grid-cols-2 justify-center items-center " id='about'>
+            <h1 className='font-extrabold text-8xl md:text-9xl md:order-1 order-2 ml-10 text-slate-50 border flex-wrap col-span-2 md:col-span-1'>About <br /> Me</h1>
+            <p className="text-3xl order-3 md:order-3 pl-10 text-slate-50 col-span-2 md:col-span-1 ">¡Hola! 👋 <br />
+                Soy Julián M. Bastidas Pérez, ingeniero de software en formación 🧑‍💻.
+                Me encanta crear cosas innovadoras y llevar ideas al siguiente nivel 🚀.
                 Soy una persona divertida, extrovertida y me apasiona programar todo lo que puedas imaginar 💡💻.
                 Vivo en la hermosa ciudad de Pasto 🏞️, y actualmente estoy cursando el sexto semestre de Ingeniería de Software.
                 Siempre que te cruces conmigo, te recibiré con mi mejor sonrisa 😁✨.</p>
-            <div className="order-1 md:order-2 border">hola</div>
-        {/* <img className="rounded-lg box-border h-25 w-25 opacity-50 order 1" src="immgAout"></img> */}
+            <div className="order-1 md:order-2 border md:row-span-2 text-center md:rounded-lg rounded-full">
+                <Image className="rounded-full md:rounded-3xl h-[110%] md:h-[500px] object-cover md:object-cover" src={ImgAbout} alt="" />
+            </div>
         </section>
         </div>
     )
