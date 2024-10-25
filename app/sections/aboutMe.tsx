2bfb7845d9d4e9c3a719163d155ba84c150c8ebe
julian <julilito09@gmail.com>
2024-10-25 14:48:33 -0500

    adicion de life

--- app/sections/aboutMe.tsx
+++ app/sections/aboutMe.tsx
@@ -14,7 +14,7 @@ const aboutMe = () => {
                 Soy una persona divertida, extrovertida y me apasiona programar todo lo que puedas imaginar 💡💻.
                 Vivo en la hermosa ciudad de Pasto 🏞️, y actualmente estoy cursando el sexto semestre de Ingeniería de Software.
                 Siempre que te cruces conmigo, te recibiré con mi mejor sonrisa 😁✨.</p>
-            <div className="pt-56 order-2 md:order-2 p-5 object-center md:row-span-2 md:rounded-lg rounded-full item-center justify-center  ">
+            <div className="md:pt-56 order-2 md:order-2 p-5 object-center md:row-span-2 md:rounded-lg rounded-full item-center justify-center  ">
                 <Image className=" rounded-full md:rounded-3xl h-[350px] w-[300px]  object-cover md:object-cover shadow-2xl shadow-black" src={ImgAbout} alt="" />
             </div> 
         </section>
--- app/sections/life.tsx
+++ app/sections/life.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import ImageLife from "../img/life.jpg";
 const life = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-1 md:grid-cols-2  ">
+            <div>
+                <Image className="border rounded-3xl md:rounded-3xl h-[350px] w-[300px]  object-cover md:object-cover shadow-2xl shadow-black" src={ImageLife} alt="" />
+            </div>
+            <div className="">
+                <h1 className=" font-extrabold text-4xl md:text-5xl  h-full text-slate-50">Story of my Life</h1>
+                <p className=""></p>
+            </div>
         </div>
     )
 }
