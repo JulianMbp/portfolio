f397d42746595388dc4d4a294ca9a3b8c0371b5c
JulianMbp <julian_mtb993@outlook.es>
2024-10-29 17:44:54 -0500

    agregacion de projecs

--- app/sections/experience.tsx
+++ app/sections/experience.tsx
@@ -1,8 +1,14 @@
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover";
+import {Tabs, Tab, Card, CardBody, CardHeader} from "@nextui-org/react";
 
 const experience = () => {
 return (
-    <section id="experience" className="flex">
-        
+    <section id="experience" className="flex h-screen w-screen items-center justify-center">
+        <section className="md:h-[70%] md:w-[80%] border">
+            <div className="flex w-full flex-col">
+                
+            </div>
+        </section>        
         {/* animate-bounce 
         para animar con saltos el boton de descarga de cv*/}
     </section>
