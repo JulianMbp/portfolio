9cf222b5454840ecf4ad06cfdcea3fba58cae497
julian <julilito09@gmail.com>
2024-11-01 18:13:01 -0500

    agregarcion de clases

--- app/sections/music.tsx
+++ app/sections/music.tsx
@@ -1,7 +1,9 @@
 const music = () => {
     return (
-        <section id="music" className="">
-                hola
+        <section id="music" className="border text-center md:w-[100%] md:p-5 items-center justify-center">
+            <dl className="border border-slate-900  md:h-[600px]">
+                Spotify
+            </dl>
         </section>
     )
 }
