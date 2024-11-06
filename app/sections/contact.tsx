2a786ffa7e8c1f766d5f72ece6861dc76203381c
JulianMbp <julian_mtb993@outlook.es>
2024-11-06 15:54:20 -0500

    adicion de modal arreglar

--- app/sections/contact.tsx
+++ app/sections/contact.tsx
@@ -1,10 +1,35 @@
 import { div } from "framer-motion/client"
+'use client';
 
-const contact = () => {
-    return (
-        <section className="hidden" id="contact">
-            
-        </section>
-    )
-}
-export default contact
+import Modal from '@/app/components/Modal';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const ModalPage: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div className='p-10 text-center'>
+      <h1 className='text-4xl mb-4'>
+        Ejemplo de Modal con Next.js y TypeScript
+      </h1>
+      <button
+        className='bg-blue-500 text-white px-4 py-2 mb-8 rounded-md'
+        onClick={openModal}
+      >
+        Abrir Modal
+      </button>
+      <br />
+      <Link href='/'>Regresar</Link>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal} title='Título del Modal'>
+        <p>Este es un ejemplo de contenido dentro del modal.</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalPage;
