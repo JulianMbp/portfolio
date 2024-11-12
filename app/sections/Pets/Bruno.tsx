0f71bece79dded4dd3e93e3e202f6ea1a0a400ad
JulianMbp <julian_mtb993@outlook.es>
2024-11-12 09:35:56 -0500

    agregar cositas de pets

--- app/sections/Pets/Bruno.tsx
+++ app/sections/Pets/Bruno.tsx
@@ -9,13 +9,14 @@ const brunoCard = () => {
         h-[150px] w-[150px] sm:h-[60%] sm:w-[90%] -mt-10 
         lg:my-10 lg:-ml-10 lg:h-[280px] lg:w-[250px]  
         object-cover lg:object-cover shadow-2xl shadow-black" src={Bruno} alt="" />
-        <dl className=" order-2 lg:w-[70%] text-center lg:text-left flex flex-col justify-center text-brunoColor-900 font-serif">
-            <h1 className="text-4xl lg:text-left ">Bruno</h1>
+        <dl className=" order-2 lg:w-[70%] text-center lg:text-left flex flex-col justify-center text-brunoColor-900 font-serif pr-10">
+            
+            <dl className="flex justify-between">
+                <h1 className="text-4xl lg:text-left ">Bruno 🍊</h1>
+                <h1 className="text-2xl  text-brunoColor-950 text-opacity-80">Edad: 1 año 10 meses</h1>
+            </dl>
             <p className=" text-brunoColor-900 text-opacity-80 lg:text-left">Gato Naranja</p>
-            <p className=" text-brunoColor-900 text-sm text-opacity-60 pb-5 px-10 lg:px-0 lg:pb-0 lg:text-left">Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
+            <p className=" text-brunoColor-900 text-sm text-opacity-60 pb-5 px-10 lg:px-0 lg:pb-0 lg:text-left">¡Hola, soy Bruno! Soy un gato naranja con panza blanca y súper juguetón. Me encantan los globos, las cobijas y, sobre todo, acurrucarme en la cama de mi humano, Julián, o en las piernas de mi abuelo. Solo acepto comida húmeda y de buena calidad; nada de baratijas. Ah, y también me fascina el pan y probar los bocaditos que comen los abuelos. ¡Soy el consentido de la casa! 🐾
             </p>
         </dl>
         </dl>
--- app/sections/Pets/Luna.tsx
+++ app/sections/Pets/Luna.tsx
@@ -10,13 +10,14 @@ const lunaCard = () => {
         h-[150px] w-[150px] sm:h-[250px] sm:w-[90%] -mt-10 
         lg:my-10 lg:-ml-10 lg:h-[280px] lg:w-[250px]  
         object-cover md:object-top lg:object-cover shadow-2xl shadow-black" src={Luna} alt="" />
-        <dl className=" order-2 lg:w-[70%] text-center lg:text-left flex flex-col justify-center text-lunaColor-950 font-serif">
-            <h1 className="text-4xl lg:text-left ">Luna</h1>
-            <p className=" text-lunaColor-950  text-opacity-80">Gato Naranja</p>
-            <p className="text-lunaColor-950   text-sm text-opacity-55 pb-5 px-10 lg:px-0 lg:pb-0">Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
-            Empowering users throug captivating interfaces, turning ideas into pixel-perfect realities.
+        <dl className=" order-2 lg:w-[70%] text-center lg:text-left flex flex-col justify-center text-lunaColor-950 font-serif pr-10">
+            <dl className="flex justify-between">
+                <h1 className="text-4xl lg:text-left ">Luna 🌙</h1>
+                <h1 className="text-2xl  text-brunoColor-950 text-opacity-80">Edad: 16 años</h1>
+            </dl>
+            <p className=" text-lunaColor-950  text-opacity-80">West highland White terrier</p>
+            <p className="text-lunaColor-900 text-sm text-opacity-60 pb-5 px-10 lg:px-0 lg:pb-0 lg:text-left">
+            ¡Hola! Soy Luna, una perrita mestiza de color blanco, con orejas paradas y un corazón lleno de amor. Aunque duermo la mayor parte del día, cuando estoy despierta me encanta disfrutar de galletas de avena, concentrado del más fino y, por supuesto, tostadas y gomas para perro. Soy la alfa de la manada y la consentida de mi mamá (pero eso queda entre nosotros). Vivir con mi familia ha sido una aventura maravillosa, y me encanta recibir caricias, ¡especialmente cuando me miman mucho!
             </p>
         </dl>
         </dl>
