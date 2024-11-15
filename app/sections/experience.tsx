41a6c74bce0fec44dd3fd9afb85b0dec96ddf514
julian <julilito09@gmail.com>
2024-11-14 22:10:32 -0500

    agregarcion cositas

--- app/sections/experience.tsx
+++ app/sections/experience.tsx
@@ -1,7 +1,4 @@
 'use client';
-import { HiAcademicCap } from "react-icons/hi2";
-import { MdWorkOutline } from "react-icons/md";
-import { PiStudentFill } from "react-icons/pi";
 import React from 'react';
 import { Tooltip } from 'react-tooltip';
 import ButtonExp  from '@/app/components/Button/ButtonExp';
@@ -14,7 +11,7 @@ import ButtonCourses from '@/app/components/Button/ButtonCourses';
 const experience = () => {
 return (
     <section id="experience" className="flex h-screen w-screen items-center justify-center">
-        <section className="h-[70%] w-[70%] border-b-4 border-r-4 border-slate-900 rounded-lg text-zinc-950">
+        <section className=" w-[70%] border-b-4 border-r-4 border-slate-900 rounded-lg text-zinc-950">
             <section className="h-[100%] w-[100%] border-t-4 border-l-4 border-slate-200 rounded-lg">
                 <section className="h-[100%] w-[100%] border-[30px] border-minecraft-border-300 rounded-sm">
                     <dl className="border-minecraft-border-950  border-4 h-[100%] w-[100%] bg-opacity-50 bg-[url('/img/texture.jpg')] bg-cover bg-center">
@@ -33,7 +30,7 @@ return (
                             <svg className="animate-bounce h-5 w-5 text-zinc-950"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />  <polyline points="7 10 12 15 17 10" />  <line x1="12" y1="15" x2="12" y2="3" /></svg>
                         </a>
                             {/* tooltip y modal */}
-                            <div className="flex flex-col justify-center items-center">
+                            <div className="flex flex-col justify-center items-center ">
                         <dl className="mt-5 transition-all flex flex-row ">
                             {/* Bachiller */}
                             <ButtonExp/>
