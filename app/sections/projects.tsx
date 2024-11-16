20cdf5c0ae8290918caa8bd0fd47a175bb3e11a6
JulianMbp <julian_mtb993@outlook.es>
2024-11-16 13:46:17 -0500

    commit desde aca

--- app/sections/projects.tsx
+++ app/sections/projects.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Tabs from "@/app/components/Tabs";
 const projects = () => {
 return (
-    <section id="projects" className="grid grid-cols-5 h-screen w-screen items-center justify-center object-center text-center">
-        <section className="border">Tecnologias</section>
-        <dl className="col-span-3">
+    <section id="projects" className="grid lg:grid-cols-5 gird md:grid-cols-2 grid-cols-1  h-screen w-screen items-center justify-center object-center text-center">
+        <section className="border">
+            Tecnologias Aprendidas
+        </section>
+        <dl className="lg:col-span-3 md:col-span-2 ">
         <Tabs/>
         </dl>
-        <section className="border">Tecnologias</section>
+        <section className="border lg:block hidden">Tecnologias</section>
     </section>
 )};
 export default projects
