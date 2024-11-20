88889d469c3b3e589dbb98779a86f28146704eb7
julian <julilito09@gmail.com>
2024-11-20 08:17:18 -0500

    primer commit

--- app/sections/Pets/Luna.tsx
+++ app/sections/Pets/Luna.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image"
-import Luna from "../../../public/img/pets/Luna.jpg"
-
+/* import Luna from "../../../public/img/pets/Luna.jpg"
+ */
 const lunaCard = () => {
     return (
         <section className="bg-lunaColor-200 h-[100%] w-[100%] rounded-3xl ">
         <dl className="rounded-3xl flex lg:gap-10 gap-5 flex-col lg:flex-row justify-center items-center">
-        <Image className=" order-1 rounded-full sm:rounded-3xl transition-all ease-in delay-300 sm:transition-all sm:ease-in sm:delay-300 
+{/*         <Image className=" order-1 rounded-full sm:rounded-3xl transition-all ease-in delay-300 sm:transition-all sm:ease-in sm:delay-300 
         md:transition-all md:ease-in md:delay-300 lg:transition-all lg:rounded-3xl
         h-[150px] w-[150px] sm:h-[250px] sm:w-[90%] -mt-10 
         lg:my-10 lg:-ml-10 lg:h-[280px] lg:w-[250px]  
-        object-cover md:object-top lg:object-cover shadow-2xl shadow-black" src={Luna} alt="" />
+        object-cover md:object-top lg:object-cover shadow-2xl shadow-black" src={} alt="" /> */}
         <dl className=" order-2 lg:w-[70%] text-center lg:text-left flex flex-col justify-center text-lunaColor-950 font-serif pr-10">
             <dl className="lg:flex lg:justify-between grid grid-cols-1 md:grid md:grid-cols-1 justify-center items-center text-center md:justify-center md:items-center md:text-center lg:text-left">
                 <h1 className="text-4xl lg:text-left ">Luna🌙</h1>
