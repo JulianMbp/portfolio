956464d7b02619ee08085997bf4fc84ba639ced2
JulianMbp <julian_mtb993@outlook.es>
2024-11-21 20:42:07 -0500

    Aplicacion final

--- app/sections/video.tsx
+++ app/sections/video.tsx
@@ -4,7 +4,7 @@ import YouTubeEmbed from '@/app/components/YouTubeEmbed';
 const Video: React.FC = () => {
 return (
     <div className='w-[70%] md:w-[50%] h-[70%] md:h-[50%] bg-cyan-200 border-2 border-black rounded-xl shadow-2xl shadow-black  md:mb-40'>
-        <YouTubeEmbed videoId='l2ABZsKHl2Y?si=SqLXAjgMyo2wL9uT' />
+        <YouTubeEmbed videoId='Jb0godyqo-s' />
     </div>
 );
 };
